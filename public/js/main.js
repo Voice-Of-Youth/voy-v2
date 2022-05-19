5765ba957ff5a767410ec88db835a9665f3db4da
nmktad <nmktadesse@gmail.com>
2022-05-19 13:24:47 +0300

    MOdified the style of searchbar and removed the expanding functionality

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,3 @@
-// Search Bar Functionalitites
-
-const searchBar = document.getElementById('search')
-const searchIcon = document.querySelector('.search-submit')
-const searchPlaceholder = document.getElementById('search-placeholder')
-
-searchIcon.addEventListener('click', ()=> {
-    searchBar.classList.toggle('is-search-active')
-    searchPlaceholder.classList.toggle('placeholder-active')
-})
-
 // Hamburger Menu Functionality
 
 const menu = document.querySelector('#mobile-bar-icon')
