365f201400d9dd1fad56abf11358c4e7befb887a
NatiShimelis <natishimelisg@gmail.com>
2022-06-06 20:53:51 +0300

    Updated the Nav bar of about us page

--- application/view/aboutus.php
+++ application/view/aboutus.php
@@ -10,12 +10,16 @@
         crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="../../public/styles/main.css">
     <link rel="stylesheet" href="../../public/styles/aboutus.css">
+    <link rel="stylesheet" type="text/css" href="../../public/styles/articles/style.css">
+	  <link rel="stylesheet" type="text/css" href="../../public/styles/main.css">
+	  <link rel="stylesheet" type="text/css" href="../../public/styles/index.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>About Us | VOY</title>
 </head>
 
 <body>
     
-  <nav class="navbar">
+<nav class="navbar">
     <div class="navbar-container">
       <a href="../../public/index.php" class="link" id="navbar-logo">VOY</a>
       
@@ -26,20 +30,20 @@
       </div>
       
       <ul class="navbar-menu menu">
-         
-      <li class="search-menu-item">
-        <form action="searchResult.php" method="POST">
-          <div class='searchContainer'>
-            <button class='iconButton' name="submit-search">
-              <i class="fa-solid fa-magnifying-glass"></i>
-            </button>
-            <input name="search" class='searchInput' placeholder='articles keyword' />
-          </div>
-        </form>
-        </li>
+        <!-- <li class="search-menu-item">
+          <form action="./searchResult.php" method="POST">
+            <div class='searchContainer'>
+              <button class='iconButton' name="submit-search">
+                <i class="fa-solid fa-magnifying-glass"></i>
+              </button>
+              <input name="search" class='searchInput' placeholder='articles keyword' />
+            </div>
+          </form>
+        </li> -->
         <?php 
           session_start();
           if(isset($_SESSION["userid"])) {
+            $image = $_SESSION["userprofileimg"];
             echo "<li class=\"navbar-menu-item\">";
             // echo "<a href='../application/view/login.php'>Bookmarks</a>";
             echo "<a href='../application/view/login.php' class='navbar-link link'>";
@@ -47,17 +51,18 @@
             echo "<span>Favorites</span>";
             echo "</a>";
             echo "</li>";
-            echo "<li id='nav-button-dynamic' class='navbar-menu-item navbar-btn'>"; 
+            // echo "<li id='nav-button-dynamic' class='navbar-menu-item navbar-btn'>"; 
             // echo "<a href='../application/view/profile.php'>Signin</a>";
-            echo "<a href='../application/view/profile.php' class='btn-link link'>Profile</a>"; 
-            echo "</li>";
-            echo "<li id='nav-button-dynamic' class='navbar-menu-item navbar-btn'>"; 
-            // echo "<a href='../application/utils/logout.utils.php'>Logout</a>";
-            echo "<form class='navbar-menu-item navbar-btn' method='post'>";
-            echo "  <button class='btn-link link' type='submit' name='logout'>Logout</button>";
-            echo "</form>";
+            // echo "<a href='../application/view/profile.php' class='btn-link link'>Profile</a>"; 
+            // echo "</li>";
+            echo "<li class= 'user-icon'> <a href= './profile.php' > <img src= '../../public/images/uploads/$image'>  </a> </li>";
+            // echo "<li id='nav-button-dynamic' class='navbar-menu-item navbar-btn'>"; 
+            // // echo "<a href='../application/utils/logout.utils.php'>Logout</a>";
+            // echo "<form class='navbar-menu-item navbar-btn' method='post'>";
+            // echo "  <button class='btn-link link' type='submit' name='logout'>Logout</button>";
+            // echo "</form>";
             // echo "<a href='../application/utils/logout.utils.php' class='btn-link link'>Log out</a>"; 
-            echo "</li>";
+            // echo "</li>";
           } else {
             echo "<li id='nav-button-dynamic' class='navbar-menu-item navbar-btn'>"; 
             // echo "<a href='../application/view/login.php'>Signin</a>";
