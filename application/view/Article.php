3f3ffc99d06a87edb3ab20ce5aab6a5101ce7d66
NatiShimelis <natishimelisg@gmail.com>
2022-06-30 00:51:11 +0300

    nav bar and footer in articles page

--- application/view/Article.php
+++ application/view/Article.php
@@ -32,37 +32,38 @@ if (!isset($_SESSION['IsWriter']) || !$_SESSION['IsWriter']) {
       
       <ul class="navbar-menu menu">
         <li class="search-menu-item">
-          <div class="navbar-link link search-styling">
-            <div id="search" class="search-bar">
-              <input type="text" class="search-text" placeholder="Search...">
-              <button class="search-submit">
+          <form action="./searchResult.php" method="POST">
+            <div class='searchContainer'>
+              <button class='iconButton' name="submit-search">
                 <i class="fa-solid fa-magnifying-glass"></i>
               </button>
-              <div class="search-dropdown" id="hits-container"></div>
+              <input name="search" class='searchInput' placeholder='articles keyword' />
             </div>
-            <span id="search-placeholder">Search</span>
-          </div>
+          </form>
         </li>
         <?php 
+        //   session_start();
           if(isset($_SESSION["userid"])) {
+            $image = $_SESSION["userprofileimg"];
             echo "<li class=\"navbar-menu-item\">";
             // echo "<a href='../application/view/login.php'>Bookmarks</a>";
-            echo "<a href='../application/view/login.php' class='navbar-link link'>";
+            echo "<a href='../../index.php' class='navbar-link link'>";
             echo "<i class='fa-solid fa-bookmark'></i>";
             echo "<span>Favorites</span>";
             echo "</a>";
             echo "</li>";
-            echo "<li id='nav-button-dynamic' class='navbar-menu-item navbar-btn'>"; 
+            // echo "<li id='nav-button-dynamic' class='navbar-menu-item navbar-btn'>"; 
             // echo "<a href='../application/view/profile.php'>Signin</a>";
-            echo "<a href='../application/view/profile.php' class='btn-link link'>Profile</a>"; 
-            echo "</li>";
-            echo "<li id='nav-button-dynamic' class='navbar-menu-item navbar-btn'>"; 
-            // echo "<a href='../application/utils/logout.utils.php'>Logout</a>";
-            echo "<form class='navbar-menu-item navbar-btn' method='post'>";
-            echo "  <button class='btn-link link' type='submit' name='logout'>Logout</button>";
-            echo "</form>";
+            // echo "<a href='../application/view/profile.php' class='btn-link link'>Profile</a>"; 
+            // echo "</li>";
+            echo "<li class= 'user-icon'> <a href= './profile.php' > <img src= '../../public/images/uploads/$image'>  </a> </li>";
+            // echo "<li id='nav-button-dynamic' class='navbar-menu-item navbar-btn'>"; 
+            // // echo "<a href='../application/utils/logout.utils.php'>Logout</a>";
+            // echo "<form class='navbar-menu-item navbar-btn' method='post'>";
+            // echo "  <button class='btn-link link' type='submit' name='logout'>Logout</button>";
+            // echo "</form>";
             // echo "<a href='../application/utils/logout.utils.php' class='btn-link link'>Log out</a>"; 
-            echo "</li>";
+            // echo "</li>";
           } else {
             echo "<li id='nav-button-dynamic' class='navbar-menu-item navbar-btn'>"; 
             // echo "<a href='../application/view/login.php'>Signin</a>";
@@ -88,68 +89,62 @@ if (!isset($_SESSION['IsWriter']) || !$_SESSION['IsWriter']) {
         </form>
     </div>
     <footer class="footer">
-        <div class="footer-container">
-            <div class="footer-links">
-                <div class="footer-link-wrapper">
-                    <div class="footer-link-items">
-                        <h2 class="footer-title">Help</h2>
-                        <a href="./FAQ.html" class="link footer-link">FAQ's</a>
-                        <a href="./report.html" class="link footer-link">Report</a>
-                    </div>
-                    <div class="footer-link-items">
-                        <h2 class="footer-title">About us</h2>
-                        <a href="#" class="link footer-link">Blogs</a>
-                        <a href="#" class="link footer-link">Testimonials</a>
-                        <a href="./aboutus.html" class="link footer-link">About Us</a>
-
-                    </div>
-                </div>
-                <div class="footer-link-wrapper">
-                    <div class="footer-link-items">
-                        <h2 class="footer-title">Careers</h2>
-                        <a href="./dashboard.php" class="link footer-link">Writers</a>
-                        <a href="#" class="link footer-link">How it works</a>
-                        <a href="./contactus.html" class="link footer-link">Contact Us</a>
-                    </div>
-                    <div class="footer-link-items">
-                        <h2 class="footer-title">Legal</h2>
-                        <a href="#" class="link footer-link">Terms</a>
-                        <a href="#" class="link footer-link">Services</a>
-                        <a href="#" class="link footer-link">Privacy</a>
-                    </div>
-                </div>
-            </div>
-            <section class="social-media">
-                <div class="social-media-wrap">
-                    <div class="footer-logo">
-                        <a href="../../index.html" class="link" id="footer-logo">VOY</a>
-                    </div>
-                    <p class="website-rights">&copy; Voice of Youth
-                        <script>
-                            document.write(new Date().getFullYear())
-                        </script>. All rights reserved
-                    </p>
-                    <div class="social-icons">
-                        <a href="#" class="social-icon-link link" target="_blank">
-                            <i class="fa-brands brand fa-facebook"></i>
-                        </a>
-
-                        <a href="#" class="social-icon-link link" target="_blank">
-                            <i class="fa-brands brand fa-instagram"></i>
-                        </a>
-
-                        <a href="#" class="social-icon-link link" target="_blank">
-                            <i class="fa-brands brand fa-twitter"></i>
-                        </a>
-
-                        <a href="#" class="social-icon-link link" target="_blank">
-                            <i class="fa-brands brand fa-telegram"></i>
-                        </a>
-                    </div>
-                </div>
-            </section>
+    <div class="footer-container">
+      <div class="footer-links">
+        <div class="footer-link-wrapper">
+          <div class="footer-link-items">
+            <h2 class="footer-title">Help</h2>
+            <a href="./faq.php" class="link footer-link">FAQ's</a>
+            <a href="./report.php" class="link footer-link">Report</a>
+          </div>
+          <div class="footer-link-items">
+            <h2 class="footer-title">About us</h2>
+            <a href="./aboutus.php" class="link footer-link">About Us</a>  
+            <a href="#" class="link footer-link">Blogs</a>
+            <a href="#" class="link footer-link">Testimonials</a>
+          </div>
         </div>
-    </footer>
+        <div class="footer-link-wrapper">
+          <div class="footer-link-items">
+          <h2 class="footer-title">Services</h2>
+            <a href="./writers.php" class="link footer-link">Authoring</a>
+          </div>
+          <div class="footer-link-items">
+            <h2 class="footer-title">Legal</h2>
+            <a href="#" class="link footer-link">Terms</a>
+            <a href="#" class="link footer-link">Services</a>
+            <a href="#" class="link footer-link">Privacy</a>
+          </div>
+        </div>
+      </div>
+      
+      <section class="social-media">
+        <div class="social-media-wrap">
+          <div class="footer-logo">
+            <a href="../../index.php" class="link" id="footer-logo">VOY</a>
+          </div>
+          <p class="website-rights">&copy; Voice of Youth <script>document.write(new Date().getFullYear())</script>. All rights reserved</p>
+          <div class="social-icons">
+            <a href="#" class="social-icon-link link" target="_blank">
+              <i class="fa-brands brand fa-facebook"></i>
+            </a>
+
+            <a href="#" class="social-icon-link link" target="_blank">
+              <i class="fa-brands brand fa-instagram"></i>
+            </a>
+
+            <a href="#" class="social-icon-link link" target="_blank">
+              <i class="fa-brands brand fa-twitter"></i>
+            </a>
+            
+            <a href="#" class="social-icon-link link" target="_blank">
+              <i class="fa-brands brand fa-telegram"></i>
+            </a>
+          </div>
+        </div>
+      </section>
+    </div>
+  </footer>
 
     <script src="../js/main.js"></script>
     <script src="../js/index.js"></script>
